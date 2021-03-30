33f754825545e9bdb0037174f3b0c7ea561311c9
Saurav Kalia <Saurav.Kalia@accesspay.com>
2021-03-30 17:48:38 +0530

    yup, formik and axios package added

--- src/components/Auth/Login/login.tsx
+++ src/components/Auth/Login/login.tsx
@@ -1,13 +1,33 @@
+import { TextField } from '@material-ui/core';
 import React from 'react';
-import { Button } from '@material-ui/core';
 import styles from './login.module.scss';
 
 type Props = {};
 
+type snakBar = {
+    open: boolean;
+    message: string | null;
+}
+
 const Login: React.FC<Props> = props => {
     return (
-        <div className={styles.constainer}>hello
-            <Button color="primary">click</Button>
+        <div className={styles.constainer}>
+            <form className={styles.form}>
+                <TextField
+                   className={styles.input}
+                   id="email"
+                   label="Email"
+                   variant="outlined"
+                   type="text"
+                   ></TextField>
+                <TextField
+                   className={styles.input}
+                   id="password"
+                   label="Password"
+                   variant="outlined"
+                   type="text"
+                   ></TextField>   
+            </form>
         </div>
     );
 }
